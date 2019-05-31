b9a03cf45959f22518e7eeea516dadd167eaa7a0
Mathieu Jourdan <ma.jourdan@hotmail.fr>
2019-05-31 15:14:27 +0200

    edit burger menu button positioning, add a gitignore file, remove .cache and .idea files from the project's versioning

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,4 +0,0 @@
-module.exports = [{
-      plugin: require('D:/Development/Projects/Perso/React/portfolio-gatsby/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
--- src/components/header.js
+++ src/components/header.js
@@ -2,27 +2,31 @@ import React from "react"
 import { Link } from "gatsby"
 import styles from './header.css'
 
-const ListLink = props => (
-    <li>
-        <Link to={props.to}>{props.children}</Link>
-    </li>
-)
-
-export default ({ children }) => (
-    <header>
-        <div className={"banner"}>
-            <img src={require('../../public/images/logos/mj-logo.png')} alt={"logo Mathieu Jourdan"}/>
-            <div className={"title"}>
-                <h1>Mathieu Jourdan</h1>
-                <h2>Junior frontend developer</h2>
-            </div>
-            <div className={"burger-menu"}></div>
-        </div>
-        <ul>
-            <ListLink to="/">About me</ListLink>
-            <ListLink to="/resume/">Resume</ListLink>
-            <ListLink to="/recent-projects/">Recent projects</ListLink>
-            <ListLink to="/contact/">Contact</ListLink>
-        </ul>
-    </header>
-)
+export default class Header extends React.Component {
+    render () {
+        const ListLink = props => (
+            <li>
+                <Link to={props.to}>{props.children}</Link>
+            </li>
+        )
+        
+        return (
+                <header>
+                    <div className={"banner"}>
+                        <img src={require('../../public/images/logos/mj-logo.png')} alt={"logo Mathieu Jourdan"}/>
+                        <div className={"title"}>
+                            <h1>Mathieu Jourdan</h1>
+                            <h2>Junior frontend developer</h2>
+                        </div>
+                        <button className={"menu-button"}></button>
+                    </div>
+                    <ul>
+                        <ListLink to="/">About me</ListLink>
+                        <ListLink to="/resume/">Resume</ListLink>
+                        <ListLink to="/recent-projects/">Recent projects</ListLink>
+                        <ListLink to="/contact/">Contact</ListLink>
+                    </ul>
+                </header>
+            )
+    }
+}
